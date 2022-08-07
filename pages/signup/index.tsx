61a76ff25864c45edda6aca96a16f3aef4eaefcb
victorEluyera <105635874+victorEluyera@users.noreply.github.com>
2022-08-07 23:01:01 +0100

    implemented signup and login pages (#3)

---
+++ pages/signup/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignUp from "../../components/signup/SignUp";
+
+function SignUpPages() {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+}
+
+export default SignUpPages;
